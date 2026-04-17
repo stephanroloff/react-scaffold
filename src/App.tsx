b67a27933d8053689884ae7ce35847501d55bbd2
Stephan Roloff <sr@werbeagenten.de>
2026-04-17 15:08:12 +0200

    start of project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+    <div className="flex w-full h-screen justify-center items-center bg-cyan-950">
+      <h1 className="text-4xl font-bold text-center text-yellow-400">React Scaffold</h1>
+    </div>
+    </>
+  );
+}
+
+export default App;
